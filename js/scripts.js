cc7dba63a6ddb44d8a4f946ffe424e3ff50ec2da
Robinson Idagiza <robinadoro@gmail.com>
2022-03-10 12:59:18 +0300

    ft:Add js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    $(".person1")
-})
+    $("#blanks form").submit(function(){
+        $(".person1").append("blah blah");
+        $(".person2").append("bleh bleh");
+        $(".animal").append("bli bli bl");
+        $(".exclamation").append("bloh bloh bloh");
+        $(".verb").append("bloh bloh bloh");
+        $(".noun").append("blu blu blu");
+
+        $("#story").show();
+        event.preventDefault();
+    });
+});
